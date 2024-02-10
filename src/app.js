cd8287eb1dfe9b47f10227635910e1c7df2d05a6
manny <157555409+manhtten@users.noreply.github.com>
2024-02-10 02:00:40 -0500

    Added Axios

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
+function searchCity(city) {
+  let apiKey = "48fbeba974f73tcce3370ao1a52849de";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial"`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
